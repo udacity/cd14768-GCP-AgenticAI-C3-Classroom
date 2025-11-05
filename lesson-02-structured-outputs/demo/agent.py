61a2416a7d7406ebb44be8ec5129ae7461053605
afirstenberg <github@addventure.com>
2025-11-04 21:05:44 -0500

    Lesson 02 demo

---
+++ lesson-02-structured-outputs/demo/agent.py
@@ -0,0 +1,33 @@
+import os
+from google.adk.agents import Agent
+from pydantic import BaseModel, Field
+from typing import Literal, Optional
+
+DirectionType = Literal["left", "right"]
+DistanceUnitsType = Literal["feet", "yards", "meters"]
+CommandType = Literal["turn", "walk", "dance", "get"]
+
+class Distance(BaseModel):
+    num: float = Field(..., description="The numerical value of the distance.")
+    units: DistanceUnitsType = Field(..., description="The units of measurement for the distance.")
+
+class RobotCommands(BaseModel):
+    command: CommandType = Field(..., description="The command for the robot.")
+    direction: Optional[DirectionType] = Field(None, description="The direction for the robot.")
+    distance: Optional[Distance] = Field(None, description="The distance for the robot to move.")
+    object: Optional[str] = Field(None, description="The object the robot should interact with.")
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+instruction_file_path = os.path.join(script_dir, "agent-prompt.txt")
+with open(instruction_file_path, "r") as f:
+    instruction = f.read()
+
+model = "gemini-2.5-flash"
+
+root_agent = Agent(
+    name="named_entity_recognition",
+    description="A tool for identifying instructions for a hypothetical robot.",
+    instruction=instruction,
+    model=model,
+    output_schema=RobotCommands,   # See what happens if we remove this line
+)
