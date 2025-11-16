fa5149299a04394da544e10e4f43b29c62492fd2
afirstenberg <github@addventure.com>
2025-11-16 12:32:23 -0500

    lesson 09 - example

---
+++ lesson-09-long-term-agent-memory/exercises/solution/agent.py
@@ -0,0 +1,35 @@
+import os
+from google.adk.agents import Agent
+from google.adk.agents.callback_context import CallbackContext
+from .search_agent import search_agent_tool
+from google.adk.tools.preload_memory_tool import preload_memory_tool
+
+agent_engine_project = os.environ.get("AGENT_ENGINE_PROJECT")
+agent_engine_location = os.environ.get("AGENT_ENGINE_LOCATION")
+agent_engine_id = os.environ.get("AGENT_ENGINE_ID")
+
+async def auto_save_session_to_memory_callback(callback_context: CallbackContext):
+    print("save session to memory")
+    await callback_context._invocation_context.memory_service.add_session_to_memory(
+        callback_context._invocation_context.session)
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+instruction_file_path = os.path.join(script_dir, "agent-prompt.txt")
+with open(instruction_file_path, "r") as f:
+    instruction = f.read()
+
+model = "gemini-2.5-flash"
+
+tools = [
+    search_agent_tool,
+    preload_memory_tool
+]
+
+root_agent = Agent(
+    name="research_assistant_agent",
+    description="An agent that can search the web and has long-term memory of past conversations.",
+    instruction=instruction,
+    model=model,
+    tools=tools,
+    after_agent_callback=auto_save_session_to_memory_callback,
+)
