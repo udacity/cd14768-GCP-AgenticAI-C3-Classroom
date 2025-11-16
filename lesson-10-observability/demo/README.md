68e53a6f02dd305ad49277a85e69bef5e4a758c4
afirstenberg <github@addventure.com>
2025-11-16 15:16:23 -0500

    lesson 10 - demo

---
+++ lesson-10-observability/demo/README.md
@@ -0,0 +1,25 @@
+# Module 10 Demo
+
+## Environment
+
+GOOGLE_GENAI_USE_VERTEXAI=TRUE
+GOOGLE_CLOUD_PROJECT=<your project ID>
+GOOGLE_CLOUD_LOCATION=us-central1
+
+OTEL_SERVICE_NAME=adk-weather
+OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED=true
+OTEL_INSTRUMENTATION_GENAI_CAPTURE_MESSAGE_CONTENT=true
+
+Make sure you replace <your project ID> with the ID for your project.
+
+Using a LOCATION of us-central1 is usually the best bet in the United States,
+but consider other cloud data center locations for elsewhere.
+
+## Instructions
+
+Make sure the Telemetry API is 
+[enabled](https://console.developers.google.com/apis/api/telemetry.googleapis.com/overview)
+
+Run this with `adk web --otel_to_cloud`
+
+Visit Google Cloud Console Trace Explorer.
---
+++ lesson-10-observability/demo/agent.py
@@ -0,0 +1,40 @@
+import os
+from google.adk.agents import Agent
+
+def get_temperature(city: str) -> str:
+    """Returns the temperature in a city."""
+    return f"The temperature in {city} is 25 degrees Celsius."
+
+def get_precipitation(city: str) -> str:
+    """Returns the precipitation in a city."""
+    return f"The precipitation in {city} is 10%."
+
+def get_wind_speed(city: str) -> str:
+    """Returns the wind speed in a city."""
+    return f"The wind speed in {city} is 15 km/h."
+
+# Experiment with the different models to see what works better
+model = "gemini-2.5-flash-lite"
+# model = "gemini-2.5-flash"
+# model = "gemini-2.5-pro"
+
+# Read the instructions from a file in the same
+# directory as this agent.py file.
+script_dir = os.path.dirname(os.path.abspath(__file__))
+instruction_file_path = os.path.join(script_dir, "agent-prompt.txt")
+with open(instruction_file_path, "r") as f:
+    instruction = f.read()
+
+tools = [
+    get_temperature,
+    get_precipitation,
+    get_wind_speed,
+]
+
+root_agent = Agent(
+    name="weather_tools",
+    description="Multiple tools to illustrate different weather conditions to return and how the ADK will use multiple tools to derive an answer",
+    model=model,
+    instruction=instruction,
+    tools=tools,
+)
